4c6680d05e31d24caa2e9e84cfe6e4d83b99fe67
marcusj12 <marcus.a.jennings12@gmail.com>
2024-07-31 11:35:31 -0400

    Initial Commit

---
+++ TransportApp/TransportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransportAppApp.swift
+//  TransportApp
+//
+//  Created by Marcus  Jennings on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TransportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
